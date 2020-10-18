2a3de1c83a7bafae5863f80419e471e8cee6b4e1
Matias Cocciolo <66866232+mattcocciolo@users.noreply.github.com>
2020-10-18 03:01:07 -0300

    Update settings.py

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -115,24 +115,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-#STATIC_URL = '/static/'
-#STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-#STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
-#STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
-#MEDIA_URL = '/images/'
-#MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images')
-
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-STATICFILE_DIRS = [
-      "static/images", 
-      "static/css", 
-      "staticfiles",]
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 MEDIA_URL = '/images/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'images')
-
-
+MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images')
 
 
 # SMTP Configuracion
