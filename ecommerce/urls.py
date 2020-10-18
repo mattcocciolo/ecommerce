5db78d5e5a29d5e9a78ae235cdb267e74126b442
Matias Cocciolo <66866232+mattcocciolo@users.noreply.github.com>
2020-10-18 01:25:39 -0300

    Update urls.py

--- ecommerce/urls.py
+++ ecommerce/urls.py
@@ -27,6 +27,3 @@ urlpatterns = [
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-urlpatterns += patterns('',(r'^static/(?P.*)$', 'django.views.static.serve', {'document_root': settings.STATIC_ROOT}),)
-
-
