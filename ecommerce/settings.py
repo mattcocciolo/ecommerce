510e03c798f2ece4086be012e70cf73ab3e74631
Matias Cocciolo <66866232+mattcocciolo@users.noreply.github.com>
2020-10-18 02:17:21 -0300

    Update settings.py

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -119,8 +119,8 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
-#MEDIA_URL = '/images/'
-#MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images')
+MEDIA_URL = '/static/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'static')
 
 # SMTP Configuracion
 
