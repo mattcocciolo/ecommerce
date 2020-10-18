758069fce5c2a9a3d6f22ea13f701662d31b1cc7
Matias Cocciolo <66866232+mattcocciolo@users.noreply.github.com>
2020-10-18 02:38:10 -0300

    Update urls.py

--- ecommerce/urls.py
+++ ecommerce/urls.py
@@ -19,10 +19,15 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
 
+from django.views.static import serve
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('store.urls')),
     path('ware/', include('ware.urls')),
+    
+    url(r'^media/(?P<path>.*)$', serve,{'document_root': settings.MEDIA_ROOT}),
+    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),   
 
 ]
 
