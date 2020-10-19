0a57c883b8c2f5435e1af69d6762fd4fb60ede0b
mattcocciolo <matiascocciolo@gmail.com>
2020-10-19 01:14:07 -0300

    eliminados elementos

--- ecommerce/static/js/main.js
+++
@@ -1,9 +0,0 @@
-
-// Esconde o muestra el usuario en la navbar dependiendo si esta o no autenticado
-if (user != 'AnonymousUser'){
-                document.getElementById('ini-session').classList.add("hidden");
-                document.getElementById('hello-user').classList.remove("hidden");
-                document.getElementById('close-session').classList.remove("hidden");
-                }
-
-
